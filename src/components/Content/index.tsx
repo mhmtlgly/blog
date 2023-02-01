5b3dc41c717cfef98940e31ca21ffd459e387f77
Mehmet Ali Gülay <mhmtlgly@gmail.com>
2023-02-01 14:03:25 +0100

    mobilemenu fullwidth

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -13,7 +13,7 @@ export const Content = ({ children }: ContentProps) => {
 
   return (
     <ReactMarkdown
-      className="max-w-full"
+      className="max-w-full mt-10"
       components={{
         code({ node, inline, className, children, style, ...props }) {
           const match = /language-(\w+)/.exec(className || "")
@@ -34,7 +34,7 @@ export const Content = ({ children }: ContentProps) => {
         },
         h2({ node, className, children, style, ...props }) {
           return (
-            <h2 className="text-xl mb-4 mt-10 first-of-type:mt-20" {...props}>
+            <h2 className="text-xl mb-4 mt-10 first-of-type:mt-0" {...props}>
               {children}
             </h2>
           )
